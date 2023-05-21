b6f8b91976bd8579a933fb518cc7798b02ac8fdd
MalcolmGuambee <134209836+MalcolmGuambee@users.noreply.github.com>
2023-05-21 15:05:32 +0200

    Add files via upload

---
+++ Python lesson.py
@@ -0,0 +1,23 @@
+#first name="malcolm"
+#last name= "guambe"
+#full name= first name +" "+ lastname
+#print("Hello"+" "+"Malcolm")
+
+#Float= Numeric Value with integers
+#int=Integer
+
+#age= 14
+#age += 1
+#print("My age is :"+str(age))
+
+#height= 170.64
+#print("My height is:" +str(height)+"cm")
+
+human= True
+print("Are you a Human:"+str(human))
+ #Recap
+ name= "Malcom"
+ age= 14
+ humble= True
+ 
+ Print(name)
